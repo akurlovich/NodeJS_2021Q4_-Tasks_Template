cb56e5ed2713ae2586ebea93b86189ffc16e0313
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-10-25 11:46:17 +0300

    feat: user services, router and repository

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,24 @@
-const getAll = async () => 
-  // TODO: mock implementation. should be replaced during task development
-   []
-;
+const USERS = [];
 
-module.exports = { getAll };
+const getAll = async () => USERS;
+const getById = async (id) => USERS.find(user => user.id === id);
+const addUser = async (user) => USERS.push(user);
+const updateUser = async (id, user) => {
+  const userIndex = USERS.findIndex(item => item.id === id);
+  if (userIndex >= 0) {
+    const userTemp = user;
+    userTemp.id = id;
+    USERS[userIndex] = userTemp;
+    return true;
+  }
+  return false;
+};
+const deleteUser = async (id) => USERS.filter((user, index) => {
+  if (user.id === id) {
+    USERS.splice(index, 1);
+    return true;
+  }
+  return false;
+});
+
+module.exports = { getAll, getById, addUser, updateUser, deleteUser };
